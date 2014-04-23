b33916aaaacae11697fe0cf14aab1af871c0c69e
Ashwin2488 <ashwin06.s@gmail.com>
2014-04-23 15:13:29 +0530

    manual bootstrapping of ng app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,35 @@
-'use strict';
+/* Name: app.js
+ * Version: 1.0
+ * Author: Prokarma
+ *
+ * -------------------------------------------------------------
+ * Copyright (c) 2012 Prokarma, Inc.
+ * All Rights Reserved
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE.
+ * The copyright notice above does not evidence any actual or
+ * intended publication of such source code.
+ * --------------------------------------------------------------
+ */
+//making sure the config is available before angular bootstrapping
 
-angular.module('phonegapAppApp', [
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+angular.element(document).ready(function() {
+  var initInjector = angular.injector(['ng']);
+  var $http = initInjector.get('$http');
+  $http.get('config/config.json').then(function(response) {
+    angular.module('phonegapAngularApp').constant('CONFIG', response.data);
+    angular.bootstrap(document, ['phonegapAngularApp']);
+
   });
+});
+
+// Define all your modules
+
+angular.module('HomeScreen', []);
+
+
+// Lastly, define your "main" module and inject all other modules as dependencies
+angular.module('phonegapAngularApp', [
+  'HomeScreen',
+  'ngRoute'
+]);
