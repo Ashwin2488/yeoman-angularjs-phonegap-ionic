1bf01de033511348aaf295bdacec5131a572189c
Ashwin2488 <ashwin06.s@gmail.com>
2014-04-30 18:45:02 +0530

    removing comments

--- app/modules/search/search-controller.js
+++ app/modules/search/search-controller.js
@@ -1,17 +1,4 @@
-/* Name: search-controller.js
- * Module: Recharge
- * Version: 1.0
- * Author: Prokarma
- *
- * -------------------------------------------------------------
- * Copyright (c) 2012 Prokarma, Inc.
- * All Rights Reserved
- *
- * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE.
- * The copyright notice above does not evidence any actual or
- * intended publication of such source code.
- * --------------------------------------------------------------
- */
+
 angular.module('Search').controller('searchController', function($scope, $rootScope,$ionicSideMenuDelegate) {
   console.log('searchController');
 
