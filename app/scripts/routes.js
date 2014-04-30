5eeb192c5154e624f15bbc7e167f0c2685faca67
Ashwin2488 <ashwin06.s@gmail.com>
2014-04-30 18:44:04 +0530

    removing comments

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -1,16 +1,4 @@
-/* Name: routes.js
- * Version: 1.0
- * Author: Prokarma
- *
- * -------------------------------------------------------------
- * Copyright (c) 2012 Prokarma, Inc.
- * All Rights Reserved
- *
- * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE.
- * The copyright notice above does not evidence any actual or
- * intended publication of such source code.
- * --------------------------------------------------------------
- */
+
 // configure our routes
 'use strict';
 angular.module('phonegapAngularApp').
