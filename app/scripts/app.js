9ed47a60b722f801f56d77da2eaa48f67ba422de
Ashwin2488 <ashwin06.s@gmail.com>
2014-04-30 18:43:40 +0530

    removing comments

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,4 @@
-/* Name: app.js
- * Version: 1.0
- * Author: Prokarma
- *
- * -------------------------------------------------------------
- * Copyright (c) 2012 Prokarma, Inc.
- * All Rights Reserved
- *
- * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE.
- * The copyright notice above does not evidence any actual or
- * intended publication of such source code.
- * --------------------------------------------------------------
- */
+
 //making sure the config is available before angular bootstrapping
 'use strict';
 angular.element(document).ready(function() {
