835a2557dd1cff6ab054e866f992f6ca3e8ae3c4
Ashwin2488 <ashwin06.s@gmail.com>
2014-04-30 18:41:51 +0530

    removing comments

--- app/modules/playlist/playlists-controller.js
+++ app/modules/playlist/playlists-controller.js
@@ -1,17 +1,4 @@
-/* Name: playlists-controller.js
- * Module: Recharge
- * Version: 1.0
- * Author: Prokarma
- *
- * -------------------------------------------------------------
- * Copyright (c) 2012 Prokarma, Inc.
- * All Rights Reserved
- *
- * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE.
- * The copyright notice above does not evidence any actual or
- * intended publication of such source code.
- * --------------------------------------------------------------
- */
+
 'use strict';
 angular.module('Playlist').controller('playlistsController', function($scope, $rootScope, $ionicSideMenuDelegate) {
   console.log('playlistsController');
